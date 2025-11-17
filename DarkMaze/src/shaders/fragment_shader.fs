7072d62f9860420286b0101aac891ebdd4416bd2
Binni-pooh <maxc0766@gmail.com>
2025-11-17 17:18:15 +0100

    Camera and gitignore

--- DarkMaze/src/shaders/fragment_shader.fs
+++ DarkMaze/src/shaders/fragment_shader.fs
@@ -20,5 +20,5 @@ void main() {
     vec3 diffuse = diff * lightColor;
 
     vec3 result = (ambient + diffuse) * objectColor;
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(objectColor, 1.0);
 }
