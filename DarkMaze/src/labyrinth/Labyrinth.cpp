7657e4822985a757919ef85112a13716ed984ca0
abalutatar <abalut.atar@gmail.com>
2025-11-18 17:34:32 +0100

    dodanie komentarzy

--- DarkMaze/src/labyrinth/Labyrinth.cpp
+++ DarkMaze/src/labyrinth/Labyrinth.cpp
@@ -2,6 +2,7 @@
 #include <stack>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 
 Labyrinth::Labyrinth() {
     for (int i = 0; i < ROWS; i++)
@@ -19,15 +20,15 @@ void Labyrinth::generateMaze() {
 
     // ROWS i COLS muszą być nieparzyste.
 
-    // Pomocnik: losuje indeks komórki należącej do siatki "komórek"
+    // losowanie indeksu komórki należącej do siatki "komórek"
     // czyli wartości 1,3,5,...,ROWS-2 (gdy ROWS jest nieparzyste).
     auto randCellIndex = [&](int maxSize) -> int {
         int count = (maxSize - 1) / 2; // np. dla 11 -> 5 (daje indeksy 1,3,5,7,9)
-        int r = rand() % count;        // 0..count-1
-        return 1 + 2 * r;              // 1,3,5,...
+        int r = rand() % count;        // 0..count-1 (0, 1, 2, 3,4)
+        return 1 + 2 * r;              // 1,3,5,...,9
         };
 
-    // 2) Wybieramy wewnętrzne komórki startowe (dla generowania) i docelowe
+    // Wybieramy wewnętrzne komórki startowe (dla generowania) i docelowe
     int startR = randCellIndex(ROWS);
     int startC = randCellIndex(COLS);
     int endR;
@@ -39,13 +40,15 @@ void Labyrinth::generateMaze() {
         endC = randCellIndex(COLS);
     } while (endR == startR && endC == startC);
 
-    // 3) Standardowy DFS (randomized) na siatce "komórkowej"
+    std::cout << startR << "   " << startC << std::endl;
+
     // Zostawiamy jedynie komórki o indeksach 1,3,5,... jako możliwe do odwiedzenia,
     // między nimi będziemy "wybijać" ściany (dr/2, dc/2).
     std::stack<std::pair<int, int>> s;
-    maze[startR][startC] = 0; // oznaczamy komórkę jako korytarz
+    maze[startR][startC] = 0; // oznaczamy komórkę startową jako korytarz
     s.push({ startR, startC });
 
+    //idziemy albo kolumnowo albo wierszowo
     int dr[4] = { -2, 2, 0, 0 };
     int dc[4] = { 0, 0, -2, 2 };
 
@@ -54,7 +57,7 @@ void Labyrinth::generateMaze() {
         s.pop();
 
         std::vector<int> dirs = { 0,1,2,3 };
-        // proste losowe przetasowanie
+        // przetasowanie
         for (int i = 0; i < 4; ++i) {
             int r = rand() % 4;
             std::swap(dirs[i], dirs[r]);
@@ -65,7 +68,7 @@ void Labyrinth::generateMaze() {
             int nr = current.first + dr[d];
             int nc = current.second + dc[d];
 
-            // sprawdź, czy nowa komórka jest wewnątrz planszy i nie odwiedzona
+            // sprawdzamy czy nowa komórka jest wewnątrz planszy i nie jest odwiedzona
             if (nr > 0 && nr < ROWS && nc > 0 && nc < COLS && maze[nr][nc] == 1) {
                 // "wybijamy" ścianę pomiędzy current a (nr,nc)
                 int wallR = current.first + dr[d] / 2;
@@ -77,77 +80,24 @@ void Labyrinth::generateMaze() {
         }
     }
 
-    // 4) Teraz wybieramy pozycje wejścia i wyjścia na obrzeżu
-    // Zrobimy wejście po lewej krawędzi powiązane z wybraną komórką
-    // oraz wyjście po prawej krawędzi powiązane z wybraną komórką.
+    //Póki co wybiera na sztywno wejscie w pierwszej kolumnie i wyjście w ostatniej (można dodać losowość)
 
-    // Wybieramy wiersze wejścia/wyjścia odpowiadające komórkom (tych, co wyżej)
+
+    // Wybieramy wiersze wejścia/wyjścia odpowiadające wierszom startowym/końcowym
     int entranceRow = startR;
     int exitRow = endR;
 
-    // Otwórz połączenie do krawędzi (po lewej i prawej)
-    // Po lewej: (entranceRow, 0) oraz (entranceRow, 1) -> 1 = komórka przy krawędzi
+    //Wybijamy w pierszej i drugiej kolumnie
     maze[entranceRow][0] = 0;
     maze[entranceRow][1] = 0; // łączymy z wnętrzem
 
-    // Po prawej: (exitRow, COLS-1) oraz (exitRow, COLS-2)
+    // //Wybijamy w ostatniej i przedostatniej kolumnie
     maze[exitRow][COLS - 1] = 0;
     maze[exitRow][COLS - 2] = 0;
 
-    // 5) Zamknij wszystkie pozostałe krawędzie (upewnij się, że reszta obwodu to ściany)
-    for (int r = 0; r < ROWS; ++r) {
-        if (r != entranceRow) maze[r][0] = 1;
-        if (r != exitRow)     maze[r][COLS - 1] = 1;
-    }
-    for (int c = 0; c < COLS; ++c) {
-        maze[0][c] = 1;
-        maze[ROWS - 1][c] = 1;
-    }
-
-    // 6) Upewnienie się: jeśli z jakiegoś powodu start/end nie połączyły się z wnętrzem,
-    // (to się raczej nie zdarzy bo ich sąsiad (1 lub COLS-2) zostały wcześniej "wyrąbane"),
-    // możemy wymusić połączenie (bezpiecznik):
-    maze[entranceRow][1] = 0;
-    maze[exitRow][COLS - 2] = 0;
 }
 
-/*
-void Labyrinth::generateMaze() {
-    srand(time(0));
-    std::stack<std::pair<int, int>> s;
-    int startRow = 0, startCol = 0;
-    maze[startRow][startCol] = 0;
-    s.push({ startRow, startCol });
-
-    int dr[4] = { -2, 2, 0, 0 };
-    int dc[4] = { 0, 0, -2, 2 };
-
-    while (!s.empty()) {
-        auto current = s.top();
-        s.pop();
-
-        std::vector<int> directions = { 0,1,2,3 };
-        for (int i = 0; i < 4; i++) {
-            int r = rand() % 4;
-            std::swap(directions[i], directions[r]);
-        }
-
-        for (int i = 0; i < 4; i++) {
-            int nr = current.first + dr[directions[i]];
-            int nc = current.second + dc[directions[i]];
-
-            if (isValid(nr, nc) && maze[nr][nc] == 1) {
-                int wallRow = current.first + dr[directions[i]] / 2;
-                int wallCol = current.second + dc[directions[i]] / 2;
-                maze[wallRow][wallCol] = 0;
-                maze[nr][nc] = 0;
-                s.push({ nr, nc });
-            }
-        }
-    }
-}
-*/
-// cubeVAO: VAO sześcianu zdefiniowany w main.cpp
+//Rysowanie labiryntu
 void Labyrinth::drawLabyrinth(Shader& shader, unsigned int cubeVAO) {
     shader.use();
 
@@ -156,8 +106,8 @@ void Labyrinth::drawLabyrinth(Shader& shader, unsigned int cubeVAO) {
         for (int j = 0; j < COLS; j++) {
             if (maze[i][j] == 1) {
                 glm::mat4 model = glm::mat4(1.0f);
-                model = glm::translate(model, glm::vec3(j * cellSize, 0.5f, i * cellSize));
-                model = glm::scale(model, glm::vec3(cellSize, 1.0f, cellSize));
+                model = glm::translate(model, glm::vec3(j * cellSize, 0.5f, i * cellSize)); //przesunięcie na poz. j,0.5, i
+                //model = glm::scale(model, glm::vec3(cellSize, 1.0f, cellSize)); //póki co skalowanie nie jest potrzebne
                 shader.setMat4("model", model);
 
                 glBindVertexArray(cubeVAO);
